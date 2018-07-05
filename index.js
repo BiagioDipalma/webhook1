dcd6f67047915dd88117b78dc20d9f937de458e2
biagiodipalma <dipalma.biagio@gmail.com>
2018-07-05 10:55:12 +0200

    prova

--- index.js
+++ index.js
@@ -11,12 +11,7 @@ restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
 
-  var speech =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.echoText
-      ? req.body.result.parameters.echoText
-      : "Seems like some problem. Speak again.";
+  var speech = "ho from webhook"
   return res.json({
     speech: speech,
     displayText: speech,
