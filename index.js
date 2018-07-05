5857f8c1446f9b3ebe46cc1c066f75b0576654a6
biagiodipalma <dipalma.biagio@gmail.com>
2018-07-05 11:42:07 +0200

    dd

--- index.js
+++ index.js
@@ -10,33 +10,10 @@ app.listen(port);
 
 server.post('/getMovies',function (request,response)  {
   if(request.body.result.parameters['CityHall']) {
-      var req = unirest("GET", "https://api.themoviedb.org/3/movie/top_rated");
-          req.query({
-              "page": "1",
-              "language": "en-US",
-              "api_key": ""
-          });
-          req.send("{}");
-          req.end(function(res) {
-              if(res.error) {
-                  response.setHeader('Content-Type', 'application/json');
-                  response.send(JSON.stringify({
-                      "speech" : "Error. Can you try it again ? ",
-                      "displayText" : "Error. Can you try it again ? "
-                    }));
-                  } else if(res.body.results.length > 0) {
-                      let result = res.body.results;
-                      let output = '';
-                      for(let i = 0; i<result.length;i++) {
-                          output += result[i].title;
-                          output+="\n"
-                      }
-                      response.setHeader('Content-Type', 'application/json');
-                      response.send(JSON.stringify({
-                          "speech" : output,
-                          "displayText" : output
-                      })); 
-                  }
-              });
-      }
+    response.send(JSON.stringify{
+      "speech": "provaConnesso?",
+      "displayText": "provaconnesso)"
+    });  
+
+    }
 });
